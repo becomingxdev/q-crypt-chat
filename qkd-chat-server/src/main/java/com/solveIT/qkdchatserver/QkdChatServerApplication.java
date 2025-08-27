a882695530973c5f4684fe0a6788eaafe497dd35
becomingxdev <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 11:31:55 +0530

    started backend

---
+++ qkd-chat-server/src/main/java/com/solveIT/qkdchatserver/QkdChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.solveIT.qkdchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QkdChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QkdChatServerApplication.class, args);
+	}
+
+}
---
+++ qkd-chat-server/src/test/java/com/solveIT/qkdchatserver/QkdChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solveIT.qkdchatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QkdChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
